14ee171546b05fcf8ad51be6ebd31bc8a91c13e2
Federico Gonzalez Brizzio <fedegonzal@gmail.com>
2022-03-21 16:01:42 -0300

    cargo correctamente bootstrap y fontawsome

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,4 @@
 window.$ = window.jQuery = require('jquery');
 window.Popper = require('popper.js');
 require('bootstrap');
+require('@fortawesome/fontawesome-free/js/all');
--- webpack.mix.js
+++ webpack.mix.js
@@ -2,9 +2,9 @@ const mix = require("laravel-mix");
 
 mix.setPublicPath('./dist')
 
-mix.js('./src/js/pages/index.js', 'js').extract(['react', 'react-dom']).react();
+//mix.js('./src/js/pages/index.js', 'js').extract(['react', 'react-dom']).react();
 
-mix.js('src/js/index.js', 'js');
+mix.js('src/js/app.js', 'js');
 
 mix.sass('./src/sass/style.scss', 'css');
 
