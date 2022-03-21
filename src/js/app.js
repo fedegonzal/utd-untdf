7797f5c30f4532ad715fec2e891e2d374dc2b579
Federico Gonzalez Brizzio <fedegonzal@gmail.com>
2022-03-21 16:47:56 -0300

    Agrego efectos AOS

--- src/js/app.js
+++ src/js/app.js
@@ -2,3 +2,8 @@ window.$ = window.jQuery = require('jquery');
 window.Popper = require('popper.js');
 require('bootstrap');
 require('@fortawesome/fontawesome-free/js/all');
+
+import AOS from 'aos';
+AOS.init({
+    once: true
+});
